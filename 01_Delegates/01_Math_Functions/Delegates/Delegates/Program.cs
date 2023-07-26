d814d15c9fde0b52b845194fc08647fec4881dcd
Arthur-Fetter <artthurfeter@gmail.com>
2023-07-26 14:39:06 -0300

    Initial Commit. Commiting Existing code base.

---
+++ 01_Delegates/01_Math_Functions/Delegates/Delegates/Program.cs
@@ -0,0 +1,15 @@
+﻿delegate int MathOperation(int x, int y);
+
+class Program
+{
+    static MathOperation Add = (a, b) => a + b;
+    static MathOperation Subtract = (a, b) => a - b;
+
+    static int Calculate(int a, int b, MathOperation f) => f(a, b);
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"The result is {Calculate(4, 2, Subtract)}!");
+    }
+    
+}
---
+++ 01_Delegates/05_Average_In_List/Average/Average/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static Func<List<int>, double> Average = (IntegerList) =>
+    {
+        double average = 0;
+
+        foreach (int num in IntegerList)
+        {
+            average = average + num;
+        }
+
+        average = average / IntegerList.Count;
+
+        return average;
+    };
+
+    
+    static void Main()
+    {
+        List<int> SomeList = new() { 1, 2, 3, 4, 5, 6, 7, 8};
+
+        double some_average = Average(SomeList);
+
+        Console.WriteLine($"Here is the average: {some_average}");
+    }
+    
+}
